e0e73516c808ddf2c02b9da7746d5bb490e07f0a
freshie-Dev <hazardpubg1122@gmail.com>
2023-09-18 21:04:44 +0500

    third commit

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const path = require('path'); // Import the 'path' module to work with file paths
+const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  // Use the 'path' module to construct the full path to the 'index.html' file
+  const indexPath = path.join(__dirname, 'index.html');
+  console.log(indexPath)
+  
+  // Send the 'index.html' file
+  res.sendFile(indexPath);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
